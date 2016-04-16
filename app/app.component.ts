36c80c5e795393b01aafe51b3af40d77fa673a3f
Glue <lordglue@gmail.com>
2016-04-16 13:52:57 +0700

    init

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>Нет, это мое приложение! 222</h1>'
 })
+
+
 export class AppComponent { }
